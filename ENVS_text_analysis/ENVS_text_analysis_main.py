8bb08f60ef7cea3e6894bc7b8b6f3524f0df2008
Finn Lowden <88800437+flowden@users.noreply.github.com>
2022-01-03 12:56:43 -0500

    Delete ENVS_text_analysis directory

--- ENVS_text_analysis/ENVS_text_analysis_main.py
+++
@@ -1,9 +0,0 @@
-python get-pip.py
-import pandas as pd
-
-# Importing datasets
-NTEE_codes = pd.read_csv(r'/Users/finn/Dropbox/CDL/Thesis/Datasets/BusinessMasterFile.csv')
-Trade_found = pd.read_csv(r'/Users/finn/Dropbox/CDL/Thesis/Datasets/NTEE_Codes.xlsx')
-
-#code
-print(NTEE_codes)
